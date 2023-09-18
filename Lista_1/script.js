ae07a9a6498c32e63ff089d5ef58702543d909df
Esteban Vanegas <juanestebanvanegasvela@gmail.com>
2023-09-18 11:57:14 -0500

    Se agrego lista de contactos al archivo script.js

---
+++ Lista_1/script.js
@@ -0,0 +1,18 @@
+//1
+let listaDeContactos = [
+  { nombreApellido: "Estebillan Vanegas" },
+  { nombreApellido: "Policarpa Salavarrieta" },
+  { nombreApellido: "Angela Camacho" },
+  { nombreApellido: "Adriana Ocampo" },
+];
+console.log(listaDeContactos.length);
+console.log("Primer contacto:" + listaDeContactos[0].nombreApellido);
+console.log("Ultimo contacto:" + listaDeContactos[3].nombreApellido);
+//2
+let nuevoContacto = { nombreApellido: "Ada Lovelace" };
+listaDeContactos.push(nuevoContacto);
+console.log(listaDeContactos);
+//3
+listaDeContactos.pop();
+//4
+console.log(listaDeContactos);
