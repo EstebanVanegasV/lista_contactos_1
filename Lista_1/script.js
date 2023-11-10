5a437fa0bd9872884e4ac7baafe764de2e13a967
Esteban Vanegas <juanestebanvanegasvela@gmail.com>
2023-11-10 15:21:11 -0500

    Se modifico el codigo en js y el html.

--- Lista_1/script.js
+++ Lista_1/script.js
@@ -1,88 +1,29 @@
-// //1
-// let listaDeContactos = [
-//   { nombreApellido: "Estebillan Vanegas" },
-//   { nombreApellido: "Policarpa Salavarrieta" },
-//   { nombreApellido: "Angela Camacho" },
-//   { nombreApellido: "Adriana Ocampo" },
-// ];
-// console.log(listaDeContactos.length);
-// console.log("Primer contacto:" + listaDeContactos[0].nombreApellido);
-// console.log("Ultimo contacto:" + listaDeContactos[3].nombreApellido);
-// //2
-// let nuevoContacto = { nombreApellido: "Ada Lovelace" };
-// listaDeContactos.push(nuevoContacto);
-// console.log(listaDeContactos);
-// //3
-// listaDeContactos.pop();
-// //4
-// console.log(listaDeContactos);
-// //5
-// const gitHub = "Vamos a Git Hub 👻";
-
-//segunda solucion
-//1
-let listaDeContactos = [
-  { id: 2, nombre: "Estebillan Vanegas", telefono: "3232898905" },
-  { id: 3, nombre: "La Pola Salavarrieta", telefono: "2345678901" },
-  { id: 4, nombre: "Angela Camacho", telefono: "3126789423" },
-  { id: 5, nombre: "Ada Lovelace", telefono: "3156783415" },
+const contactos = [
+  { nombre: "Juan", apellido: "Vanegas" },
+  { nombre: "Esteban", apellido: "Vela" },
+  { nombre: "Camila", apellido: "Vanegas" },
 ];
-console.log(listaDeContactos);
 
-function agregarContacto(nombre, telefono) {
-  let nuevoContacto = { nombre: "Adriana Ocampo", telefono: "3256784128" };
-  listaDeContactos.push(nuevoContacto);
-  console.log(`Contacto ${nombre} agregado.`);
-}
-function eliminarContacto(nombre) {
-  let indice = listaDeContactos.findIndex(
-    (contacto) => contacto.nombre === nombre
-  );
-  if (indice !== -1) {
-    listaDeContactos.splice(indice, 1);
-    console.log(`Contacto ${nombre} eliminado.`);
-  } else {
-    console.log(`El contacto ${nombre} no fue encontrado.`);
-  }
-}
-function imprimirConsola() {
-  console.log("Lista de contactos:");
-  listaDeContactos.forEach((contacto) => {
-    console.log(`${contacto.nombre} - Telefono: ${contacto.telefono}`);
+function imprimirContactos() {
+  console.log("Contactos:");
+  contactos.forEach((contacto) => {
+    console.log(`${contacto.nombre} ${contacto.apellido}`);
   });
 }
-//5
-const gitHub = "Subiendo a Git Hub";
 
-//Lista de contactos #2
-const crearContacto = {
-  id: 1,
-  nombres: "Juan Esteban",
-  apellidos: "Vanegas Vela",
-  telefono: 3175453697,
-  ubicacion: { a: "Mosquera Cundinamarca", b: "cra 7 # 12-56" },
-};
-function agregarNuevo(contacto) {
-  listaDeContactos.push(contacto);
+function agregarContacto(nombre, apellido) {
+  const nuevoContacto = { nombre, apellido };
+  contacto.push(nuevoContacto);
+  console.log(`Se ha añadido a ${nombre} ${apellido} a la lista.`);
 }
-agregarNuevo(crearContacto);
 
-//review funcion actualizar
-function actualizarContacto(id, nuevoContacto) {
-  const contactoExistente = listaDeContactos.find(
-    (contacto) => contacto.id === id
-  );
-  if (!contactoExistente) {
-    return "no se encontro contacto con ese id";
+function borrarContacto(index) {
+  if (index >= 0 && index < contactos.length) {
+    const contactoEliminado = contactos.splice(index, 1)[0];
+    console.log(
+      `Se ha eliminado a ${contactoEliminado.nombre} ${contactoEliminado.apellido} de la lista`
+    );
+  } else {
+    console.error("Indice de contacto invalido.");
   }
-  contactoExistente.nombre = nuevoContacto.nombre;
-  contactoExistente.telefono = nuevoContacto.telefono;
-  return "Contacto actualizado";
 }
-const nuevoContacto = {
-  id: 2,
-  nombre: "Carol Leon",
-  telefono: 3176252408,
-};
-const resultado = actualizarContacto(nuevoContacto.id, nuevoContacto);
-console.log(resultado);
