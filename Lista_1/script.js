7cc05a3d7e27bbddd48a2936005cc7e40933a3c4
Esteban Vanegas <juanestebanvanegasvela@gmail.com>
2023-11-10 15:47:14 -0500

    Se modifico el codigo para darle unos parametros mas especificos

--- Lista_1/script.js
+++ Lista_1/script.js
@@ -1,24 +1,54 @@
 const contactos = [
-  { nombre: "Juan", apellido: "Vanegas" },
-  { nombre: "Esteban", apellido: "Vela" },
-  { nombre: "Camila", apellido: "Vanegas" },
+  {
+    id: 1,
+    nombres: "Juan",
+    apellidos: "Vanegas",
+    telefono: "telefono1",
+    ubicacion: { ciudad: "Ciudad1", direccion: "Direccion1" },
+  },
+  {
+    id: 2,
+    nombres: "Esteban",
+    apellidos: "Vela",
+    telefono: "telefono2",
+    ubicacion: { ciudad: "Ciudad2", direccion: "Direccion2" },
+  },
+  {
+    id: 3,
+    nombres: "Camila",
+    apellidos: "Vanegas",
+    telefono: "telefono3",
+    ubicacion: { ciudad: "Ciudad3", direccion: "Direccion3" },
+  },
 ];
 
 function imprimirContactos() {
   console.log("Contactos:");
   contactos.forEach((contacto) => {
-    console.log(`${contacto.nombre} ${contacto.apellido}`);
+    console.log(
+      `${contacto.id}. ${contacto.nombre} ${contacto.apellido}, Telefono: ${contacto.telefono}, Ubicacion: ${contacto.ubicacion.ciudad}, ${contacto.ubicacion.direccion}`
+    );
   });
 }
 
-function agregarContacto(nombre, apellido) {
-  const nuevoContacto = { nombre, apellido };
+function agregarContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
+  const nuevoContacto = {
+    id,
+    nombres,
+    apellidos,
+    telefono,
+    ubicacion: {
+      ciudad,
+      direccion,
+    },
+  };
   contacto.push(nuevoContacto);
   console.log(`Se ha añadido a ${nombre} ${apellido} a la lista.`);
 }
 
-function borrarContacto(index) {
-  if (index >= 0 && index < contactos.length) {
+function borrarContacto(id) {
+  const index = contactos.findIndex((contacto) => contacto.id === id);
+  if (index !== -1) {
     const contactoEliminado = contactos.splice(index, 1)[0];
     console.log(
       `Se ha eliminado a ${contactoEliminado.nombre} ${contactoEliminado.apellido} de la lista`
